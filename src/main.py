8e277bbd787d4d559975113ddd48ccb65ab5bc50
jorge06br <jorgebaca06@gmail.com>
2022-08-05 22:19:02 +0000

    [USER AND PEOPLE GET]

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -21,22 +21,52 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_single_user(user_id):
+
+    if user_id==0:
+        raise APIException("No existe un usuario con ID 0",status_code=500)
+    user =User.query.get(user_id)
+    if user is None:
+        raise APIException("El usuario con ese ID no existe",status_code=400)
+    response_body={
+        "Usuario":user.serialize()
     }
 
+    return jsonify(response_body), 200
+
+@app.route('/User',methods=['POST'])
+def post_new_user():
+    body = request.get_json()
+    if body['email'] is none:
+        raise APIException('Necesitas llenar los campos de email')
+    response_body={
+        
+        "msg":"El usuario has sido creado"
+    }
+
+    return jsonify(response_body),200
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+
+    response_body = {
+    "msg": "Hello, this is your GET /user response "
+    }
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
--- src/models.py
+++ src/models.py
@@ -17,3 +17,27 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class people(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(20), unique=False, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
+    mass = db.Column(db.Integer, unique=False, nullable=False)
+    eye_color = db.Column(db.String(30), unique=False, nullable=False)
+    skin_color = db.Column(db.String(30), unique=False, nullable=False)
+    hair_color = db.Column(db.String(30), unique=False, nullable=False)
+    birth_year=db.Column(db.Integer, unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<people %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender":self.gender,
+            "description":self.description
+            
+        }
